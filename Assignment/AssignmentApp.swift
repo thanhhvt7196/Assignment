23c3d2c7f5aea1c5d64f6ababd5f71ee1f2b8a58
thanhhvt7196 <thanhhvt7196@gmail.com>
2025-04-22 20:40:01 +0700

    Initial Commit

---
+++ Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by thanh tien on 22/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
